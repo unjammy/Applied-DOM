dc51fd7428170565a07957e0cb94091a2ddc0750
clarkdenunzio <clark@clarkdenunzio.com>
2017-06-03 19:22:09 -1000

    button 5

--- js/app.js
+++ js/app.js
@@ -118,6 +118,7 @@
    *   and passing two aditional arguments, event and 'light-green'
    */
 
+   btn5.addEventListener("click", (event) => setMyLightClass.apply(btn5, [event, 'light-green']) );
 
   /*
    * Add a click event listener to btn6
