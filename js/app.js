a84832a6898f94d0de930f3b3fe37281d1da9950
clarkdenunzio <clark@clarkdenunzio.com>
2017-06-03 19:24:46 -1000

    button 6

--- js/app.js
+++ js/app.js
@@ -129,6 +129,9 @@
    * to set the context to the correct object (the current context)
    */
 
+   btn6.addEventListener("click", function(){
+    setMyLightGreen.call(btn6);
+   });
 
   /*
    * Add a click event listener to btn7
