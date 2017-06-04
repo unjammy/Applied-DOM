9e8e71c1e54f7c80996a2a0bba772e3ce2a49d4e
clarkdenunzio <clark@clarkdenunzio.com>
2017-06-03 19:32:10 -1000

    button 10

--- js/app.js
+++ js/app.js
@@ -180,6 +180,7 @@
    * to set the context to the btn10 object
    */
 
+   const setLight10Green = setMyLightGreen.bind(btn10);
 
   /*
    * Add a click event listener to btn10
@@ -188,6 +189,9 @@
    * will invoke the setLight10Green function
    */
 
+   btn10.addEventListener("click", function(){
+    setLight10Green();
+   });
 
   /*
    * Declare a new const named setLight11Green
